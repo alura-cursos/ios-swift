bc92805faede863598618182ac6de6475e18c6a6
guilherme <guilherme.silveira@caelum.com.br>
2014-11-15 22:05:14 -0500

    first controller for adding meals

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -1,23 +1,17 @@
-//
-//  ViewController.swift
-//  eggplant-brownie
-//
-//  Created by Guilherme Silveira on 11/15/14.
+//  Created by Guilherme Silveira
 //  Copyright (c) 2014 alura. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var happinessField: UITextField!
+    
+    @IBAction func add() {
+        let name = nameField.text
+        let happiness = "5"
+        println("eaten: \(name) \(happiness)!")
     }
 
 

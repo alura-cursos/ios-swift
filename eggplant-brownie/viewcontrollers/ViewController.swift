16fedc4f51951957cda126f10b80cce28e7f5105
guilherme <guilherme.silveira@caelum.com.br>
2014-11-17 17:40:36 -0500

    7b-navigation-pop

--- eggplant-brownie/viewcontrollers/ViewController.swift
+++ eggplant-brownie/viewcontrollers/ViewController.swift
@@ -21,6 +21,10 @@ class ViewController: UIViewController {
 
         let meal = Meal(name: name, happiness: happiness!)
         println("eaten: \(meal.name) \(meal.happiness)")
+
+        if let navigation = self.navigationController {
+            navigation.popViewControllerAnimated(true)
+        }
     }
 
 }

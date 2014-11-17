aa7ab46c215333c030111897ea9792c59d19053c
guilherme <guilherme.silveira@caelum.com.br>
2014-11-17 18:28:22 -0500

    9-items-table

--- eggplant-brownie/viewcontrollers/ViewController.swift
+++ eggplant-brownie/viewcontrollers/ViewController.swift
@@ -6,12 +6,36 @@ import UIKit
 protocol AddAMealDelegate {
     func add(meal: Meal)
 }
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     
+    var items = [ Item(name: "Eggplant Brownie", calories: 10),
+        Item(name: "Zucchini Muffin", calories: 10),
+        Item(name: "Cookie", calories: 10),
+        Item(name: "Coconut oil", calories: 500),
+        Item(name: "Chocolate frosting", calories: 1000),
+        Item(name: "Chocolate chip", calories: 1000)
+    ]
+
     @IBOutlet var nameField: UITextField!
     @IBOutlet var happinessField: UITextField!
     var delegate:AddAMealDelegate?
-    
+
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return items.count
+    }
+
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let row = indexPath.row
+            let item = items[ row ]
+            var cell = UITableViewCell(style:
+                UITableViewCellStyle.Default,reuseIdentifier: nil)
+            cell.textLabel.text = item.name
+            return cell
+    }
+
     @IBAction func add() {
         if nameField == nil || happinessField == nil {
             return

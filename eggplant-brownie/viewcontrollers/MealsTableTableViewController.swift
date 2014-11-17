9a08fe380b22d3cc335e8877a1a57b0ad7412268
guilherme <guilherme.silveira@caelum.com.br>
2014-11-17 17:28:25 -0500

    6-eggplant-table

---
+++ eggplant-brownie/viewcontrollers/MealsTableTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MealsTableTableViewController.swift
+//  eggplant-brownie
+//
+//  Copyright (c) 2014 alura. All rights reserved.
+//
+
+import UIKit
+
+class MealsTableTableViewController: UITableViewController {
+
+    var meals = [ Meal(name: "Eggplant brownie", happiness: 5),
+        Meal(name: "Zucchini Muffin", happiness: 3)]
+
+    override func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return meals.count
+    }
+
+    override func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            let row = indexPath.row
+            let meal = meals[ row ]
+            
+            var cell = UITableViewCell(style: UITableViewCellStyle.Default,
+                reuseIdentifier: nil)
+            cell.textLabel.text = meal.name
+            return cell
+    }
+}

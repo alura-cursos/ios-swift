65b1e9baa0de887a101fe7972163ef3479e04b43
guilherme <guilherme.silveira@caelum.com.br>
2014-11-21 03:59:28 -0500

    10-c-creating-a-new-item

--- eggplant-brownie/viewcontrollers/NewItemViewController.swift
+++ eggplant-brownie/viewcontrollers/NewItemViewController.swift
@@ -2,34 +2,26 @@
 //  NewItemViewController.swift
 //  eggplant-brownie
 //
-//  Created by Guilherme Silveira on 11/18/14.
 //  Copyright (c) 2014 alura. All rights reserved.
 //
 
 import UIKit
 
 class NewItemViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet var nameField:UITextField?
+    @IBOutlet var caloriesField:UITextField?
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func addNewItem() {
+        if nameField == nil || caloriesField == nil {
+            return
+        }
+        let name = nameField!.text
+        let calories = NSString(string: caloriesField!.text).doubleValue
+        
+        let item = Item(name: name, calories: calories)
+        if let navigation = navigationController {
+            navigation.popViewControllerAnimated(true)
+        }
     }
-    */
 
 }

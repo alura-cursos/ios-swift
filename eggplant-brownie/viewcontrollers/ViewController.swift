d6ea5aff9161721bc26a1b64cac40e463c1956b6
guilherme <guilherme.silveira@caelum.com.br>
2014-11-24 12:50:02 -0200

    14-c-items

--- eggplant-brownie/viewcontrollers/ViewController.swift
+++ eggplant-brownie/viewcontrollers/ViewController.swift
@@ -8,13 +8,7 @@ protocol AddAMealDelegate {
 }
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, AddAnItemDelegate {
     
-    var items = [ Item(name: "Eggplant Brownie", calories: 10),
-        Item(name: "Zucchini Muffin", calories: 10),
-        Item(name: "Cookie", calories: 10),
-        Item(name: "Coconut oil", calories: 500),
-        Item(name: "Chocolate frosting", calories: 1000),
-        Item(name: "Chocolate chip", calories: 1000)
-    ]
+    var items = Array<Item>()
 
     @IBOutlet var nameField: UITextField!
     @IBOutlet var happinessField: UITextField!
@@ -25,6 +19,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func addNew(item: Item) {
         items.append(item)
+        let dir = getUserDir()
+        let archive = "\(dir)/eggplant-brownie-items"
+        NSKeyedArchiver.archiveRootObject(items, toFile: archive)
         if let table = tableView {
             table.reloadData()
         } else {
@@ -65,12 +62,26 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
     
+    func getUserDir() -> String {
+        let userDir = NSSearchPathForDirectoriesInDomains(
+            NSSearchPathDirectory.DocumentDirectory,
+            NSSearchPathDomainMask.UserDomainMask,
+            true)
+        return userDir[ 0 ] as String
+    }
+
     override func viewDidLoad() {
         let newItemButton = UIBarButtonItem(title: "new item",
             style: UIBarButtonItemStyle.Plain,
             target: self,
             action: Selector("showNewItem"))
         navigationItem.rightBarButtonItem = newItemButton
+        let dir = getUserDir()
+        let archive = "\(dir)/eggplant-brownie-items"
+
+        if let loaded = NSKeyedUnarchiver.unarchiveObjectWithFile(archive) {
+            items = loaded as Array
+        }
     }
     
     @IBAction func showNewItem() {

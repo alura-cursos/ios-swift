acc104b6ba6b55348d787f568cc2b835960c5e71
guilherme <guilherme.silveira@caelum.com.br>
2014-11-24 12:41:38 -0200

    14-a-serialize-deserialize

--- eggplant-brownie/models/Item.swift
+++ eggplant-brownie/models/Item.swift
@@ -7,12 +7,23 @@
 
 import Foundation
 
-class Item: Equatable {
+class Item: NSObject, Equatable, NSCoding {
+
     let name:String
     let calories:Double
     init(name: String, calories: Double) {
         self.name = name
         self.calories = calories
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        self.name = aDecoder.decodeObjectForKey("name") as String
+        self.calories = aDecoder.decodeDoubleForKey("calories")
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(self.name, forKey: "name")
+        aCoder.encodeDouble(self.calories, forKey: "calories")
     }
 }
 func ==(first:Item, second:Item) -> Bool {
--- eggplant-brownie/models/Meal.swift
+++ eggplant-brownie/models/Meal.swift
@@ -6,13 +6,19 @@
 
 import Foundation
 
-class Meal {
+class Meal : NSObject, NSCoding {
     let name:String
     let happiness:Int
     var items = Array<Item>()
     init(name: String, happiness: Int) {
         self.name = name
         self.happiness = happiness
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        self.name = aDecoder.decodeObjectForKey("name") as String
+        self.happiness = aDecoder.decodeIntegerForKey("happiness")
+        self.items = aDecoder.decodeObjectForKey("items") as Array<Item>
     }
     
     func allCalories() -> Double {
@@ -33,5 +39,12 @@ class Meal {
         
         return message
     }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(self.name, forKey: "name")
+        aCoder.encodeInteger(self.happiness, forKey: "happiness")
+        aCoder.encodeObject(self.items, forKey: "items")
+    }
+
 
 }
